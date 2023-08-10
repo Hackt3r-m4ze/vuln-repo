e634dff8fca40767fdb3a389aef893c2f8886e28
SathvikaMondu <123160305+SathvikaMondu@users.noreply.github.com>
2023-08-10 12:11:42 +0530

    Updated API keys

--- exposed.js
+++ exposed.js
@@ -31,3 +31,15 @@ fetch('https://example.com', {
 .catch(err => {
     console.error(err);
 })
+
+
+Sample API Keys
+
+bitly - R_a1bc23def456gh78i9j01234k567l8m
+github - ghp_0123456789abcdef0123456789abcdef012345
+dropbox - 7v0sdjkwj7h4rx9
+digitalocean - 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef
+heroku - 012345678-9abc-9abc-9abc-0123456789ab
+trello - b4c356789d2a1e3f6a7b8c9d0e2f1a3b
+virustotal - c1a2b3d4e5f6g7h8i9j0k1l2m3n4o5p6q
+google geo location - AIzaSyB39EXAMPLEFw0G6YcXOTKSHI8e0uTO4
